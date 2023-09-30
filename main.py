78929ce385457303277c24e420d241dda5e4cf28
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-09-30 18:37:26 -0400

    now script requests path to source file, generates name for a new translated file. Added few docstrings.

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ from contextlib import ExitStack
 from typing import Iterable, Iterator, List, Any
 import pandas as pd
 import csv
+import re
 from googletrans import Translator
+from memory_profiler import profile
+from pandas import DataFrame
 
-PATH_TO_SOURCE_FILE = "./notebooks/data500.csv"  # input("Please enter path to source CSV file")
+# GLOBAL VARIABLES
+
+PATH_TO_SOURCE_FILE = ""  # "./notebooks/data194k.csv"  # input("Please enter path to source CSV file")
 TARGET_LANGUAGE = "eng"  # input("Please enter target language, i.e. 'eng' ")
 PATH_TO_TRANSLATION_FILE = "./.translation.csv"
-PATH_TO_FINAL_FILE = "./data3_translated.csv"
+PATH_TO_FINAL_FILE = ""
 CHOICES_MADE = "0,1"
 GOOGLE_LENGTH = 4000
 LINES_PER_REQUEST = 150  # количество строк, по которым происходит итерация за один запуск цикла.
 TARGET_HEADER = 'Объекты административно-территориального деления,^ кроме сельских населенных пунктов'
 TARGET_HEADER_ENG = "Объекты деления ENG"
 SPLIT_KEY = "\n"
+
+# PARSER AND PARAMETERS
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-ps", "--path_source",
@@ -30,7 +37,7 @@ parser.add_argument("-pf", "--path_final",
                     type=str)
 parser.add_argument("-lpr", "--lines_per_request",
                     help=f"number of column rows that will be packed in one package"
-                            f"with the total max length of {GOOGLE_LENGTH} symbols ",
+                         f"with the total max length of {GOOGLE_LENGTH} symbols ",
                     type=int)
 args = parser.parse_args()
 if args.path_source:
@@ -47,9 +54,16 @@ if args.lines_per_request:
 logging.basicConfig(level=logging.INFO)
 
 
-def check_file(path_to_file="") -> str or FileNotFoundError:
+# CODE
+
+def check_file(path_to_file: str = "") -> str or FileNotFoundError:
+    """
+    function checks if path to file is not zero-length or asks for a path if else. Then, it checks if file exists
+    @param path_to_file: path to a file
+    @return: returns the same string if file exists or FileNotFound error if file is missing
+    """
     if len(path_to_file) == 0:
-        path_to_file = input()
+        path_to_file = input("please enter path to source file:\n")
 
     if not os.path.exists(path_to_file):
         print("wrong path to source file")
@@ -58,7 +72,19 @@ def check_file(path_to_file="") -> str or FileNotFoundError:
     return path_to_file
 
 
+def make_filename(source_name) -> str:
+    path_name = re.split(".csv", source_name)[0]
+    new_path_name = path_name + str("_TRANSLATED.csv")
+
+    return new_path_name
+
+
 def get_random_time() -> float:
+    """
+    a crude implementation of time randomizer in order to
+    emulate a human being pauses between requests to Google Translate
+    @return: float
+    """
     timer_seconds = [0.5, 0.75, 1.0, 2.0, 2.5]
     shuffle = random.choice(timer_seconds)
     return shuffle
@@ -77,17 +103,30 @@ def timer_func(func):
     return wrap_func
 
 
-def create_final_file_name(path_to_sf: str, path_to_tf: str):
+def create_final_file_name(path_to_sf: str, path_to_tf: str) -> DataFrame:
+    """
+    creates joined pd.DataFrame if lengths of both source file and translation file
+    are equal.
+    @param path_to_sf: path to source file, string
+    @param path_to_tf: path to translation file, string
+    @return: pd.DataFrame containing source file with a translated column
+    """
     source_file = pd.read_csv(path_to_sf)
     translation_file = pd.read_csv(path_to_tf)
     if verify_lens(source_file, translation_file):
-        final_file = source_file.join(translation_file)
+        final_file: DataFrame = source_file.join(translation_file)
         return final_file
     else:
         raise ValueError
 
 
-def verify_lens(source_file, translation_file):
+def verify_lens(source_file: DataFrame, translation_file: DataFrame) -> bool:
+    """
+    verifies if lenghts of both source and translation files are equal
+    @param source_file:
+    @param translation_file:
+    @return: bool
+    """
     if len(source_file) == len(translation_file):
         return True
     else:
@@ -95,12 +134,16 @@ def verify_lens(source_file, translation_file):
 
 
 @timer_func
-def pack_to_string(pack_components: list) -> str:
+def pack_to_string(pack_components: Iterable) -> str:
+    """
+    packs listed iterables into a string limited in length by the global value of GOOGLE_LENGTH
+    @param pack_components: Iterable
+    @return: string
+    """
     packed_string = ""
-    print("type(pack_components)", type(pack_components))
-    for v in pack_components:
+    for value in pack_components:
         if len(packed_string) < GOOGLE_LENGTH:
-            packed_string += f"{v}{SPLIT_KEY} "
+            packed_string += f"{value}{SPLIT_KEY} "
 
         else:
             break
@@ -110,6 +153,11 @@ def pack_to_string(pack_components: list) -> str:
 
 @timer_func
 def translate_pack(_packed_string: str or None) -> str or None:
+    """
+    takes a string and translates it from any Google Translate language into English
+    @param _packed_string: string or None
+    @return: string or None
+    """
     translator = Translator()
     if _packed_string is None:
         translated_string = None
@@ -121,6 +169,11 @@ def translate_pack(_packed_string: str or None) -> str or None:
 
 @timer_func
 def unpack_string(packed_string: str) -> list[str]:
+    """
+    unpacks string separated by a global variable SPLIT_KEY into a list of strings
+    @param packed_string: string to be separated
+    @return: list of strings
+    """
     string_to_list = packed_string.strip().split(SPLIT_KEY)
     string_cleared = [item.strip() for item in string_to_list]
 
@@ -128,11 +181,12 @@ def unpack_string(packed_string: str) -> list[str]:
 
 
 @timer_func
-def iterate_batches(iterable_data: Iterator, batch_size: int) -> dict:
+def iterate_batches(iterable_data: Iterator, batch_size: int) -> list:
     """
-
+    Simple iterator limited by a batch size
     @param batch_size: int
     @type iterable_data: object
+    @return: list
     """
     flag = True
     while flag:
@@ -159,10 +213,11 @@ def get_column_data(_batch: list, header) -> list:
 
 
 if __name__ == "__main__":
-    path = check_file(PATH_TO_SOURCE_FILE)
+    path_to_source = check_file(PATH_TO_SOURCE_FILE)
+    path_to_final_file = make_filename(path_to_source)
 
     with ExitStack() as stack:
-        csvfile = stack.enter_context(open(path, "r+", newline=''))
+        csvfile = stack.enter_context(open(path_to_source, "r+", newline=''))
         csvfile2 = stack.enter_context(open(PATH_TO_TRANSLATION_FILE, "w+", newline=''))
         dict_reader = csv.DictReader(csvfile)
         csv_writer = csv.writer(csvfile2)
@@ -171,11 +226,8 @@ if __name__ == "__main__":
         batches = iterate_batches(dict_reader, LINES_PER_REQUEST)
         for batch in batches:
             column_batch = get_column_data(batch, TARGET_HEADER)
-            print("len(column_batch)", len(column_batch))
             pack = pack_to_string(column_batch)
-            # print("len(pack)",len(pack))
             translated_pack = translate_pack(pack)
-            # print("len(translated_pack)",len(translated_pack))
             sleeping_timer = get_random_time()
             print(f"timer set to: {sleeping_timer}")
             time.sleep(sleeping_timer)
@@ -190,11 +242,9 @@ if __name__ == "__main__":
                     f'\n unpacked_string {unpacked_string} \n column_batch  {column_batch}')
                 raise ValueError
 
-            # print("len(unpacked_string)",len(unpacked_string))
-
             for x in unpacked_string:
                 csv_writer.writerow([x, ])
 
-    final_file = create_final_file_name(PATH_TO_SOURCE_FILE, PATH_TO_TRANSLATION_FILE)
+    final_file = create_final_file_name(path_to_source, PATH_TO_TRANSLATION_FILE)
     print(final_file, sep="\n")
-    final_file.to_csv(PATH_TO_FINAL_FILE)
+    final_file.to_csv(path_to_final_file)
